ce0da3ae4b2b4c15f491a58d70ea9d68765a1578
John Pettit <john.pettit@cox.net>
2020-04-16 15:02:45 -0700

    Initial

---
+++ src/main/java/com/greenwavesystems/actuatorpromtest/ActuatorpromtestApplication.java
@@ -0,0 +1,13 @@
+package com.greenwavesystems.actuatorpromtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorpromtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorpromtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenwavesystems/actuatorpromtest/ActuatorpromtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenwavesystems.actuatorpromtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorpromtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
